8c7ef0516359992a000b9a616b40f7494787eb90
Maksim Mihailovskii <maximmmd13@gmail.com>
2026-01-12 12:52:04 +0200

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.13
+            /*Console.Write("Введите координату на плоскости x=");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите координату на плоскости y=");
+            int y = int.Parse(Console.ReadLine());
+            if (x > 0 && y > 0)
+                Console.WriteLine("Точка А в первой четверти.");
+            else if (x < 0 && y > 0)
+                Console.WriteLine("Точка А во второй четверти.");
+            else if (x < 0 && y < 0)
+                Console.WriteLine("Точка А в третьей четверти.");
+            else if (x < 0 && y < 0)
+                Console.WriteLine("Точка А в четвёртой четверти.");
+            else if (x == 0 && y == 0)
+                Console.WriteLine("Находиться на пересечении двух осей");
+            else if (x == 0 )
+                Console.WriteLine("Находиться на оси X");
+            else 
+                Console.WriteLine("Находиться на оси Y");*/
+
+            //1.17
+            /*Console.Write("Введите количество рублей на поездку A=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите продолжительность пути S=");
+            int s = int.Parse(Console.ReadLine());
+            Console.Write("Введите стоимость бензина за литр K=");
+            int k = int.Parse(Console.ReadLine());
+            if (a >= s / 10 * k)
+                Console.WriteLine("Хватит денег чтобы доехать");
+            else
+                Console.WriteLine("Не хватит денег чтобы доехать");*/
+
+
+            //1.21
+            /*Console.Write("Введите a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Введите c=");
+            int c = int.Parse(Console.ReadLine());
+            if(a<b && a<c)
+                Console.WriteLine($"Наименьшее число первого порядка");
+            else if (c<b )
+                Console.WriteLine($"Наименьшее число третьего порядка");
+            else
+                Console.WriteLine($"Наименьшее число второго порядка");*/
+
+        }
+    }
+}
