5d1462b86ced6da4c2d5bb7d93f3c02965f48030
Maksim Mihailovskii <maximmmd13@gmail.com>
2026-01-15 16:07:01 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4.1-3
+            Console.WriteLine("Введите число k");
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число n");
+            int n = int.Parse(Console.ReadLine());
+            long res = Fact(n)/ (Fact(k)*Fact(k-n));
+            Console.WriteLine($"Биномиальный коэфицент = {res}");
+            static long Fact(int n)
+            {
+                long f = 1;
+                for (int i = 2; i <= n; i++)
+                    f *= i;
+                    return f;
+            }
+        }
+    }
+}
