60948c3b4f7d02c18aa881ca81e4ee8845176e53
Maksim Mihailovskii <maximmmd13@gmail.com>
2026-01-15 14:47:24 +0200

    Add files via upload

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -52,8 +52,10 @@ namespace ConsoleApp1
                 Console.WriteLine($"Наименьшее число третьего порядка");
             else
                 Console.WriteLine($"Наименьшее число второго порядка");*/
+
+
             //1.3
-            Console.WriteLine("-------------------------");
+            /*Console.WriteLine("-------------------------");
             Console.WriteLine("   м\tсажени\tаршины");
             Console.WriteLine("-------------------------");
             int a;
@@ -61,7 +63,56 @@ namespace ConsoleApp1
             {
                 Console.WriteLine($"   {a}\t{a*2.1336:f3}\t{a*0.7112:f3}");
             }
-            Console.ReadLine();
+            Console.ReadLine();*/
+
+
+            //2.3
+            /*Console.Write("Введите количество районов n=");
+            int n = int.Parse(Console.ReadLine());
+            double total_area = 0;
+            for (int x=1; x <= n; x++)
+            {
+                Console.Write($"Введите количество тыс. человек в {x} районе =");
+                int a=int.Parse(Console.ReadLine());
+                Console.Write($"Введите плотность(тыс. чел./км) населения в {x} районе =");
+                int b = int.Parse(Console.ReadLine());
+                double area = a / b;
+                total_area = total_area + area;
+            }
+            Console.WriteLine($"Общая площадь территории {total_area}");
+            Console.ReadLine();*/
+
+
+            //3.3
+            /*Console.Write("Введите степендию студента a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите расходы на проживание b=");
+            double b = int.Parse(Console.ReadLine());
+            int x;
+            for (x=1; a>b; x++)
+            {
+                b = b + b * 0.05;
+            }
+            Console.WriteLine($"На {x} месяц студенту надо помощь родителей");*/
+
+
+            //4.3
+            /*Console.Write("Введите трёхзначное число n=");
+            int n = int.Parse(Console.ReadLine());
+            int d=0;
+            for (int x = 100; x < n; x++)
+            {
+                int a = x / 100;
+                int b = (x / 10) % 10;
+                int c = x % 10;
+
+                if (a!=b && b!=c) 
+                {
+                    Console.Write($"{x}\t");
+                    d++;
+                }
+            }
+            Console.WriteLine($"Количество чисел {d}");*/
         }
     }
 }
