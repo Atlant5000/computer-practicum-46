859584326945a967b49cab14270733b17a4949c2
Maksim Mihailovskii <maximmmd13@gmail.com>
2026-01-17 09:55:34 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,20 +6,46 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //4.1-3
-            Console.WriteLine("Введите число k");
+           //4.1-3
+            /*Console.WriteLine("Введите число k");
             int k = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите число n");
             int n = int.Parse(Console.ReadLine());
-            long res = Fact(n)/ (Fact(k)*Fact(k-n));
+            long res = Fact(n) / (Fact(k) * Fact(k - n));
             Console.WriteLine($"Биномиальный коэфицент = {res}");
             static long Fact(int n)
             {
                 long f = 1;
                 for (int i = 2; i <= n; i++)
                     f *= i;
-                    return f;
+                return f;
+            }*/
+            //4.3-3
+            /*Console.Write("Введите начальное значение x0=");
+            int x0 = int.Parse(Console.ReadLine());
+            Console.Write("Введите множитель a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите приращение b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Введите модуль m=");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество случайно сгенерированных чисел n=");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Псевдослучайные числа: ");
+            int x = x0;
+            for (int i = 0; i < n; i++)
+            {
+                x = LCG(x, a, b, m, 1);
+                Console.Write($"{x}\t");
             }
+            static int LCG(int x, int a, int b, int m, int k)
+            {
+                if (k == 0)
+                    return x;
+                return LCG((a * x + b) % m, a,b,m,k-1);
+            }*/
+            
         }
     }
 }
+
